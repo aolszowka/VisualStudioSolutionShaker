f0e608d4a2822d99475eca95d39c16a9b67f1ead
Ace Olszowka <aolszowka@gmail.com>
2020-11-13 13:37:52 -0700

    Update SolutionUtilitiesTests.cs

--- VisualStudioSolutionShaker.Tests/SolutionUtilitiesTests.cs
+++ VisualStudioSolutionShaker.Tests/SolutionUtilitiesTests.cs
@@ -6,7 +6,9 @@
 
 namespace VisualStudioSolutionShaker.Tests
 {
+    using System;
     using System.Collections;
+    using System.Collections.Generic;
     using System.IO;
 
     using NUnit.Framework;
@@ -23,6 +25,14 @@ namespace VisualStudioSolutionShaker.Tests
             Assert.That(actual, Is.EqualTo(expected));
         }
 
+        [TestCaseSource(typeof(GetProjectsFromSolutionExceptDependencies_ValidInput_Tests))]
+        public void GetProjectsFromSolutionExceptDependencies_ValidInput(string solutionFile, IEnumerable<string> expected)
+        {
+            IEnumerable<string> actual = SolutionUtilities.GetProjectsFromSolutionExceptDependencies(solutionFile);
+
+            Assert.That(actual, Is.EquivalentTo(expected));
+        }
+
         [TestCaseSource(typeof(TryGetDepedenciesFolderGuid_ValidInput_Tests))]
         public void TryGetDepedenciesFolderGuid_ValidInput(string solutionFile, string expected)
         {
@@ -33,11 +43,27 @@ namespace VisualStudioSolutionShaker.Tests
         }
     }
 
+    internal class GetProjectsFromSolutionExceptDependencies_ValidInput_Tests : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new TestCaseData
+                (
+                    Path.Combine(TestContext.CurrentContext.TestDirectory, "TestData", "net472_A", "net472_A.sln"),
+                    new string[]
+                    {
+                        Path.Combine(TestContext.CurrentContext.TestDirectory, "TestData", "net472_A", "net472_AA", "net472_AA.csproj")
+                    }
+                );
+        }
+    }
+
     internal class TryGetDepedenciesFolderGuid_ValidInput_Tests : IEnumerable
     {
         public IEnumerator GetEnumerator()
         {
             yield return new TestCaseData(Path.Combine(TestContext.CurrentContext.TestDirectory, "TestData", "net472_A", "net472_A.sln"), "{5AD1934C-0503-4768-9385-B27CC2DE023F}");
+            yield return new TestCaseData(Path.Combine(TestContext.CurrentContext.TestDirectory, "TestData", "net472_B", "net472_B.sln"), "{15A19F88-B2A7-457A-8251-DE41846A74BB}");
         }
     }
 
@@ -46,6 +72,7 @@ namespace VisualStudioSolutionShaker.Tests
         public IEnumerator GetEnumerator()
         {
             yield return new TestCaseData("  {A2CA68C7-43CB-4E31-A10E-BDF28DAFB512} = {0D1BC702-FE2E-4C44-B710-E7CC66E5D5FD}", "{A2CA68C7-43CB-4E31-A10E-BDF28DAFB512}");
+            yield return new TestCaseData("{65A803D1-AA8B-43CB-9BEB-CBFE282EB653} = {15A19F88-B2A7-457A-8251-DE41846A74BB}", "{65A803D1-AA8B-43CB-9BEB-CBFE282EB653}");
         }
     }
 }
